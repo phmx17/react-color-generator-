ab83f842c8d14a6f7753ecad0681ffa7824ed463
Slim Yelow <slimyelow@gmail.com>
2021-06-21 12:37:02 +0200

    add section tags and set state vars

--- src/App.js
+++ src/App.js
@@ -2,9 +2,22 @@ import React, { useState } from 'react'
 import SingleColor from './SingleColor'
 
 import Values from 'values.js'
+const [color, setColor] = useState('')
+const [error, setError] = useState(false)
+const [list, setList] = useState([])
+
+const handleSubmit = (e) => {
+  e.preventDefault()
+  console.log('hello');
+}
 
 function App() {
-  return <h2>color generator project</h2>
+  return <>
+    <section className="container">
+      
+    </section>
+    <section className="colors" ></section>
+  </>
 }
 
 export default App
