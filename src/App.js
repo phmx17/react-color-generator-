d7d9925a5c9684c82d782894d7f9d51fa02fdb72
Slim Yelow <slimyelow@gmail.com>
2021-06-21 15:21:36 +0200

    add SingleColor component and implement rgbToHex()

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
 import React, { useState } from 'react'
 import SingleColor from './SingleColor'
 
-import Values from 'values.js'
+import Values from 'values.js'  // value library
+
+
+function App() {
 const [color, setColor] = useState('')
 const [error, setError] = useState(false)
 const [list, setList] = useState([])
 
 const handleSubmit = (e) => {
   e.preventDefault()
-  console.log('hello');
+  try {
+    let colors = new Values(color).all(10)    
+    setList(colors)
+  } catch (error) {
+    setError(true)  // this flag is used to create class for red border around input field
+    console.log(error);    
+  }
 }
-
-function App() {
   return <>
     <section className="container">
-      
+      <h3>Color Generator</h3>
+      <form onSubmit={handleSubmit}> 
+        <input 
+          type="text" 
+          value={color} 
+          onChange={(e)=>setColor(e.target.value)} 
+          placeholder="#f15025" 
+          className={`${error ? 'error' : null}`} />
+        <button className="btn" type="submit">submit</button>
+      </form>
     </section>
-    <section className="colors" ></section>
+    <section className="colors">
+      {list.map((color, index) => {
+        console.log(color);
+        return <SingleColor key={index} {...color} index={index} />
+      })}
+
+    </section>
   </>
 }
 
--- src/SingleColor.js
+++ src/SingleColor.js
@@ -1,8 +1,17 @@
 import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
-const SingleColor = () => {
-  return <h4>single color</h4>
+const SingleColor = ({rgb, weight, index}) => {
+  const [alert, setAlert] = useState(false)
+  const bgc = rgb.join(',')
+  const hex = rgbToHex(...rgb)
+  
+  return <article className={`color`} style={{backgroundColor: `rgb(${bgc})`}}>
+    {rgb}
+    <p className="percent-value">{weight}%</p>
+    <p className="color-value">{hex}</p>
+
+  </article>
 }
 
 export default SingleColor
